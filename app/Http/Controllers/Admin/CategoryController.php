2af14823878ee55e32691bf8064d94a49a2917e4
Irvan Syapar <irfansyapar007@gmail.com>
2025-07-19 23:44:31 +0800

    first commit

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    public function index(){
+        $categories = Category::get();
+        return view('admin.category.index', compact('categories'));
+    }
+
+    public function create(){
+        return view('admin.category.create');
+    }
+
+    public function store(Request $request){
+        Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]);
+        flash()->success('Category created successfully!');
+        return redirect('/admin/category');
+    }
+
+    public function edit(){
+        $category = Category::get();
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function update(Request $request, $id){
+        $category = Category::find($id);
+        $category->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]);
+        flash()->success('Category Updated successfully!');
+        return redirect('/admin/category');
+    }
+
+    public function delete(Request $request, $id){
+        $category = Category::find($id)->delete();
+        flash()->success('Category Deleted successfully!');
+        return redirect('/admin/category');
+    }
+}
---
+++ app/Models/WisataFacility.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WisataFacility extends Model
+{
+    protected $fillable = ['wisata_id', 'facility_id'];
+}
